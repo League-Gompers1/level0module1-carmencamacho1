193e3ca200b850613299d5332f8951467f2e4539
carmencamacho1 <carmen.camacho@jointheleague.org>
2017-05-15 14:58:21 -0700

    Add files via upload

---
+++ For_Loop_Guantlet.java
@@ -0,0 +1,36 @@
+package for_loops;
+
+public class For_Loop_Guantlet {
+	public static void main(String[] args) {
+		// for (int i = 0; i <= 100; i = i + 1) {
+		// System.out.println(i);
+		// }
+		// for (int j = 100; j >= 0; j = j - 1) {
+		// System.out.println(j);
+		// }
+		// for(int k = 2;k<=100;k =k+2){
+		// System.out.println(k);
+		// }
+		// for(int l = 1; l<=99;l=l+2){
+		// System.out.println(l);
+		// }
+		// for (int m = 1; m <= 500; m = m + 1) {
+		// System.out.print(m + " ");
+		// if (m % 2 == 0) {
+		// System.out.println("even");
+		// } else {
+		// System.out.println("odd");
+		// }
+		// }
+		// for (int n = 0; n <= 777; n = n + 7) {
+		// System.out.println(n);
+		// }
+		for (int o = 2004; o <= 2017; o = o + 1) {
+			if (o == 2017) {
+				System.out.println("This year," + o + ", I am " + (o - 2004) + " years old");
+			} else {
+				System.out.println("In " + o + ", I was " + (o - 2004) + " years old");
+			}
+		}
+	}
+}
